f816f675ebac6add64e4916977948dd5435596d8
mar1n <30738216+mar1n@users.noreply.github.com>
2020-04-26 16:49:32 +0100

    re-render react

--- src/index.js
+++ src/index.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+import * as serviceWorker from './serviceWorker'
 import store from './store'
+import { getReadableStories } from './selectors/story'
 import { STORY_ARCHIVE } from './constants/actionTypes'
-import storyReducer from './reducers/story';
+//import storyReducer from './reducers/story';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App 
-      stories={store.getState().storyState}
-      onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })} />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function render() {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App 
+        stories={getReadableStories(store.getState())}
+        onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })}
+      />
+    </React.StrictMode>,
+    document.getElementById('root')
+  )
+}
+
+store.subscribe(render)
+render()
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
 
 
 if (module.hot) {
